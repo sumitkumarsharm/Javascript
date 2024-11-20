9e06cbaddb55f729833a2acd61369bd119a4cfcc
Naman Sharma <snaman2748@gmail.com>
2024-11-21 00:07:48 +0530

    Here i Learn Loops in javascript

---
+++ Lecture3/script.js
@@ -0,0 +1,19 @@
+// for (let i = 1; i < 10; i++) {
+//   console.log("I love javascript");
+// }
+// console.log("");
+
+// let i = 1;
+// while (i < 10) {
+//   console.log("I love Javascript");
+//   i++;
+// }
+
+let i = 1;
+while (i < 10) {
+  if (i == 5) {
+    i++;
+    continue;
+  } else console.log(i);
+  i++;
+}
