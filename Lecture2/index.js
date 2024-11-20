61dc505b15799ecb302bf6699943531764ed0e9f
Naman Sharma <snaman2748@gmail.com>
2024-11-21 00:06:52 +0530

    This Lecture i learn what is Operator

---
+++ Lecture2/index.js
@@ -0,0 +1,99 @@
+// // Number
+// let firstNumber1 = 25;
+// let SecondNumber1 = 254;
+// let result1 = firstNumber1 + SecondNumber1;
+// console.log(result1); // Output: 279
+
+// // 2 Subtraction
+// let firstNumber2 = 25;
+// let SecondNumber2 = 254;
+// let result2 = firstNumber2 - SecondNumber2;
+// console.log(result2); // Output: -229
+
+// // Multiplication
+// let firstNumber3 = 25;
+// let SecondNumber3 = 254;
+// let result3 = firstNumber3 * SecondNumber3;
+// console.log(result3); // Output: 6350
+
+// // Division;
+// let firstNumber4 = 25;
+// let SecondNumber4 = 254;
+// let result4 = firstNumber4 / SecondNumber4;
+// console.log(result4); // Output: 0.09803921568627451
+
+// // modulus
+// let firstNumber5 = 25;
+// let SecondNumber5 = 254;
+// let result5 = firstNumber5 % SecondNumber5;
+// console.log(result5); // Output: 25
+
+// // power function
+// let firstNumber6 = 5;
+// let SecondNumber6 = 4;
+// let result6 = Math.pow(firstNumber6, SecondNumber6);
+// console.log(result6); // Output: 1.4190625e+66
+
+// // pre and post increment
+// // Pre increment
+// let num1 = 7;
+// ++num1;
+// console.log(num1);
+
+// // post increment
+// num1++;
+// console.log(num1);
+
+// // pre and post decrement
+// // Pre decrement
+// let num2 = 8;
+// --num2;
+// console.log(num2);
+// num2--;
+// console.log(num2);
+
+// // Assigment Operator
+// //   =
+// let A = 87;
+
+// // +=
+// // let B = 45;
+// // B += 10;
+// // console.log(B); // Output: 55\
+
+// // comparision Operator
+// // ternary operator
+// let age = 25;
+// let result7 =
+//   age > 18 ? console.log("Go for vote") : console.log("Age is too low");
+// // logical operator
+// // AND operator (both condition is must be true)
+// let Age = 7;
+// let result8 =
+//   Age > 18 && Age < 100
+//     ? console.log("Age is valid")
+//     : console.log("not valid Age");
+
+// // Or(||)
+// let PersonAge = 44;
+// let PersonAge1 = 44;
+// let PersonAge2 = 44;
+// let PersonAge3 = 44;
+// let result9 =
+//   PersonAge > 76 || PersonAge1 > 897 || PersonAge2 > 99 || PersonAge3 > 87
+//     ? console.log("Hello world")
+//     : console.log("dead world");
+
+// // not(!)
+// // let Name = "";
+// // const result10 = !Name ? console.log("enter name") : console.log("Hello");
+
+// // conditionals
+// // if-else
+// let age = 89;
+// if (age > 18) {
+//   console.log("vote");
+// } else {
+//   console.log("! vote");
+// }
+// // switch case
---
+++ Lecture2/readme.md
@@ -0,0 +1,100 @@
+# Operators in JavaScript
+
+JavaScript is a dynamic programming language, has various operators to perform operations on data and manipulate values.
+
+## Types of operators
+
+Understanding JavaScript operators is the most basic thing for writing efficient and expressive code. By mastering these operators, we'll gain the tools needed to manipulate data, make decisions, and create more dynamic and interactive applications.
+
+- 🌟 Arithmetic Operators
+
+- 📍Assignment Operators
+
+- 🌟Comparison Operators
+
+- 📍Logical Operators
+
+- 🌟Bitwise Operators
+
+- 📍Ternary Operators
+
+### Arithmetic Operators
+
+Arithmetic operators in JavaScript allow us to perform mathematical calculations such as addition, subtraction, multiplication, division, and modulus. These operators are important for performing basic mathematical calculations within our code.
+
+1. Addition
+
+```
+let firstNumber = 25;
+let SecondNumber = 254;
+let result = firstNumber + SecondNumber;
+console.log(result); // Output: 279
+```
+
+2. Subtraction
+
+```
+let firstNumber = 25;
+let SecondNumber = 254;
+let result = firstNumber - SecondNumber;
+console.log(result); // Output: -229
+```
+
+3. Multiplication
+
+```
+let firstNumber = 25;
+let SecondNumber = 254;
+let result = firstNumber * SecondNumber;
+console.log(result); // Output: 6350
+```
+
+4. Division
+
+```
+let firstNumber = 25;
+let SecondNumber = 254;
+let result = firstNumber / SecondNumber;
+console.log(result); // Output: 0.09803921568627451
+```
+
+5. Modulus
+
+```
+let firstNumber = 25;
+let SecondNumber = 254;
+let result = firstNumber % SecondNumber;
+console.log(result); // Output: 25
+```
+
+6. Exponentiation
+
+```
+let firstNumber = 25;
+let SecondNumber = 254;
+let result = Math.pow(firstNumber, SecondNumber);
+console.log(result); // Output: 1.4190625e+66
+```
+
+7. Increment
+
+```
+let firstNumber = 25;
+let result = firstNumber++;
+console.log(result); // Output: 25
+console.log(firstNumber); // Output: 26
+```
+
+8. Decrement
+
+```
+let firstNumber = 25;
+let result = firstNumber--;
+console.log(result); // Output: 25
+console.log(firstNumber); // Output: 24
+`
+```
+
+## Comparision Operator
+## Ternary Operator
+
